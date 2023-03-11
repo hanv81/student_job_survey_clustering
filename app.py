ec4e2bbf942c2eef477d358c1998461d3e5b1a1f
hanv81 <58970248+hanv81@users.noreply.github.com>
2023-03-11 23:36:49 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pickle
+
+temp = st.number_input('Input temperature', value=20)
+if st.button('Predict'):
+  st.caption('Predict Revenue')
+  model = pickle.load(open('model.pickle', "rb"))
+  rev = model.predict([temp])
+  st.success(f'{rev[0]}')
