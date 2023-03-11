336e74874451bed5f3d3762660841a0d3f8d409b
hanv81 <58970248+hanv81@users.noreply.github.com>
2023-03-11 23:51:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ temp = st.number_input('Input temperature', value=20)
 if st.button('Predict'):
   st.caption('Predict Revenue')
   model = pickle.load(open('model.pickle', "rb"))
-  rev = model.predict([temp])
+  rev = model.predict([[temp]])
   st.success(f'{rev[0]}')
